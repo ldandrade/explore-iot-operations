27cda773441a037f58e49968b027a9decce50f1f
Lucas de Andrade Santos <lucas.andrade@oilandgames.com>
2025-03-31 18:45:07 +0100

    Comment EMQx standalone clis and add K3D/K8s deployment config

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -6,13 +6,16 @@ set -o pipefail
 
 echo "Starting On Create Command"
 
-# Copy the custom first run notice over
+# Copy the custom first-run notice over
 sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
-# Create k3d cluster and forwarded ports
+# Delete existing cluster and recreate it
 k3d cluster delete
 k3d cluster create \
--p '1883:1883@loadbalancer' \
--p '8883:8883@loadbalancer'
+-p '1883:1883@loadbalancer' \  # AIO MQTT Broker
+-p '8883:8883@loadbalancer' \  # AIO Secure MQTT Broker
+-p '1884:1884@loadbalancer' \  # EMQX MQTT Broker (NEW)
+-p '8884:8884@loadbalancer' \  # EMQX Secure MQTT Broker (NEW)
+-p '18084:18083@loadbalancer'  # EMQX Dashboard (NEW)
 
 echo "Ending On Create Command"
