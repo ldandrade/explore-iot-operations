f0817d9e078c57e2fa4b23d63791cd5a3db967b8
Lucas de Andrade Santos <lucas.andrade@oilandgames.com>
2025-03-31 19:28:39 +0100

    Minor improvements in onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -19,11 +19,11 @@ fi
 k3d cluster delete || echo "No cluster found to delete"
 
 # Create the cluster with corrected port mappings
-k3d cluster create \
--p "1883:1883@loadbalancer" \  # AIO MQTT Broker
--p "8883:8883@loadbalancer" \  # AIO Secure MQTT Broker
--p "1884:1884@loadbalancer" \  # EMQX MQTT Broker (NEW)
--p "8884:8884@loadbalancer" \  # EMQX Secure MQTT Broker (NEW)
--p "18084:18083@loadbalancer"  # EMQX Dashboard (NEW)
+k3d cluster create "$CLUSTER_NAME" -p "1883:1883@loadbalancer" -p "8883:8883@loadbalancer" -p "1884:1884@loadbalancer" -p "8884:8884@loadbalancer" -p "18084:18083@loadbalancer"
+# AIO MQTT Broker 1883
+# AIO Secure MQTT Broker 8883
+# EMQX MQTT Broker (NEW) 1884
+# EMQX Secure MQTT Broker (NEW) 8884
+# EMQX Dashboard (NEW) 18084
 
 echo "Ending On Create Command"
