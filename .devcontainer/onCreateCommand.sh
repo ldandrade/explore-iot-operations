a20b500619785f1b74980a5ee415938f2b65a7e8
Lucas de Andrade Santos <lucas.andrade@oilandgames.com>
2025-03-31 18:59:40 +0100

    Minor improvements in onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -9,13 +9,19 @@ echo "Starting On Create Command"
 # Copy the custom first-run notice over
 sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
+# Check if $CLUSTER_NAME is set, otherwise default to "k3s-default"
+if [ -z "$CLUSTER_NAME" ]; then
+  CLUSTER_NAME="k3s-default"
+  echo "No cluster name provided, defaulting to $CLUSTER_NAME"
+fi
+
 # Delete existing cluster and recreate it
-k3d cluster delete
-k3d cluster create \
--p '1883:1883@loadbalancer' \  # AIO MQTT Broker
--p '8883:8883@loadbalancer' \  # AIO Secure MQTT Broker
--p '1884:1884@loadbalancer' \  # EMQX MQTT Broker (NEW)
--p '8884:8884@loadbalancer' \  # EMQX Secure MQTT Broker (NEW)
--p '18084:18083@loadbalancer'  # EMQX Dashboard (NEW)
+k3d cluster delete || echo "No cluster found to delete"
+k3d cluster create "$CLUSTER_NAME" \
+    -p '1883:1883@loadbalancer' \  # AIO MQTT Broker
+    -p '8883:8883@loadbalancer' \  # AIO Secure MQTT Broker
+    -p '1884:1884@loadbalancer' \  # EMQX MQTT Broker (NEW)
+    -p '8884:8884@loadbalancer' \  # EMQX Secure MQTT Broker (NEW)
+    -p '18084:18083@loadbalancer'  # EMQX Dashboard (NEW)
 
 echo "Ending On Create Command"
